b9337831597f49bb6e5d4a19bacb631e3e7cc698
Daolinzhou <466118115@qq.com>
2019-12-28 22:12:47 -0800

    add LU and LinearSystem

--- main_linear_system.py
+++ main_linear_system.py
@@ -1,4 +1,5 @@
 from playLA.LinearSystem import LinearSystem
+from playLA.LinearSystem import inv
 from playLA.Matrix import Matrix
 from playLA.Vector import Vector
 
@@ -9,4 +10,34 @@ if __name__ == '__main__':
     ls.gauss_jordan_elimination()
     ls.fancy_print()
 
+    print()
+
+    # 测试更加一般化的高斯约旦消元法
+    A2 = Matrix([[1, -1, 2, 0, 3],
+                 [-1, 1, 0, 2, -5],
+                 [1, -1, 4, 2, 4],
+                 [-2, 2, -5, -1, -3]])
+    b2 = Vector([1, 5, 13, -1])
+    ls2 = LinearSystem(A2, b2)
+    ls2.gauss_jordan_elimination()
+    ls2.fancy_print()
+
+    print()
+
+    A3 = Matrix([[2, 2],
+                 [2, 1],
+                 [1, 2]])
+    b3 = Vector([3, 2.5, 7])
+    ls3 = LinearSystem(A3, b3)
+    if not ls3.gauss_jordan_elimination():
+        print("No solution")
+    ls3.fancy_print()
+
+    print()
+
+    A = Matrix([[1, 2], [3, 4]])
+    invA = inv(A)
+    print(invA)
+    print(invA.dot(A))
+    print(A.dot(invA))
 
